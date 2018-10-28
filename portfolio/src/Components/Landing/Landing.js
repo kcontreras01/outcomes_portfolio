eb91041eb1e168079cb9437cedfef9724571215a
kcontreras01 <kcontreras01@gmail.com>
2018-10-27 20:14:16 -0400

    styling mobile landing

--- portfolio/src/Components/Landing/Landing.js
+++ portfolio/src/Components/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './Landing.css'
+import Typing from 'react-typing-animation';
 
 class Landing extends Component {
   componentWillMount() {
@@ -10,26 +11,19 @@ class Landing extends Component {
     document.removeEventListener('scroll', this.onScroll);
   }
 
-			// <div className="intro">
-			// 	<h1 className="logo">KIARA CONTRERAS</h1>
-			// 	<h2>Full Stack Web Developer</h2>
-			// 	<h3>New York</h3>
-			// </div>
 	render() {
 		return (
-			// <section id="landing">
-
-
 			<section className="landing">
-        <h1>Hi! I'm Kiara.</h1>
+			<Typing>
+			  <Typing.Delay ms={500} />
+        <h1>HI! <Typing.Delay ms={900} />I'M KIARA.</h1>
+      </Typing>
+      <div id="landing-div">
         <p>I'm a passionate Full Stack Developer from NYC. </p>
         <p>Thanks for stopping by. Check out some of my most recent work below.
 				<i className="fas fa-level-down-alt jump"></i></p>
-      </section>  
-
-
-
-			// </section>
+			</div>
+      </section>
 		);
 	}
 }
